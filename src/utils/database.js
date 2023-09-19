6f0382b998a18c9ff86ea490ce2df948e40c45ee
GerardoAcosta1 <gerardonuevo1234321@gmail.com>
2023-09-18 23:22:05 -0700

    se corrigió database, errores tipográficos varios

--- src/utils/database.js
+++ src/utils/database.js
@@ -3,14 +3,14 @@ import "dotenv/config";
 
 const db = new Sequelize({
    
-    hots: process.env.DB_HOST,
+    host: process.env.DB_HOST,
     database: process.env.DB_NAME,
     port: process.env.DB_PORT,
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     dialect: 'postgres',
     ...process.env.NOD_ENV == 'production' ? {
-        dialecOptions: {
+        dialectOptions: {
             ssl:{
                 required: true,
                 rejectUnauthorized: false
